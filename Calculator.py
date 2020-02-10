3e553e622012936bcfad0a753ec5901df485d9b0
Vishalini14 <47360512+Vishalini14@users.noreply.github.com>
2020-02-09 23:14:31 -0800

    Lab 1

---
+++ Calculator.py
@@ -0,0 +1,12 @@
+n1=int(input("Enter a number "))
+n2=int(input("Enter a number "))
+
+add=n1+n2
+sub=n1-n2
+mul=n1*n2
+div=n1//n2
+
+print("Addition: ",add)
+print("Subtractiontion: ",sub)
+print("Multiplicationn: ",mul)
+print("Division of {0} and {1} is {2}".format(n1,n2,div))
