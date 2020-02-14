a38c0d8321418d524457a4d328431b1a074d16bd
Vishalini14 <47360512+Vishalini14@users.noreply.github.com>
2020-02-14 01:31:04 -0800

    Lab 2

--- PalindromeString.py
+++ PalindromeString.py
@@ -1,6 +1,6 @@
-inpString=input("Enter a string: ")
-inpString=inpString.casefold()
-revString=reversed(inpString)
+inpString = input("Enter a string: ")
+inpString = inpString.casefold()
+revString = reversed(inpString)
 if list(inpString) == list(revString):
     print(f"{inpString.capitalize()} is a Palindrome.")
 else:
