a2bdcceede17daf73e955bb4356394fee6f9414b
Vishalini14 <47360512+Vishalini14@users.noreply.github.com>
2020-02-14 00:43:39 -0800

    Lab 2

--- Largest3.py
+++ Largest3.py
@@ -1,6 +1,6 @@
-n1=input("Enter first number: ")
-n2=input("Enter second number: ")
-n3=input("Enter third number: ")
+n1=int(input("Enter first number: "))
+n2=int(input("Enter second number: "))
+n3=int(input("Enter third number: "))
 
 if(n1>=n2) and (n1>=n3):
     large=n1
